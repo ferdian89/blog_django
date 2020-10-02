9f013c3b0fb84e338ebd38c60d11bbfc3572e8ed
Your Name <you@example.com>
2020-10-02 21:23:51 +0700

    edit database

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'dj_database_url.config(conn_max_age=600, ssl_require=True)',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
